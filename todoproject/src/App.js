921772680849396d65cc644973aa255f2a1e11b9
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-06-11 09:29:20 +0530

    FINISHED TODO REACT REDUX PROJECT

--- todoproject/src/App.js
+++ todoproject/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { addTodo, deleteTodo } from "./slices/todoSlice";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { todos } = useSelector((state) => state.todo);
+  console.log(inputText, todos);
+
+  const onAddClick = () => {
+    dispatch(
+      addTodo({
+        id: uuid(),
+        todo: inputText,
+      })
+    );
+    setInputText("");
+  };
+
+  const onDeleteClick = (id) => {
+    dispatch(deleteTodo({ id }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-200 w-screen h-screen">
+      <h1 className="text-purple-950 pt-4">Todo App</h1>
+      <div>
+        <input
+          placeholder="Enter a todo..."
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button onClick={onAddClick}>Add</button>
+      </div>
+      {todos?.length > 0 &&
+        todos.map((todo) => (
+          <div>
+            <span>{todo.todo}</span>
+            <button onClick={() => onDeleteClick(todo.id)}>Delete</button>
+          </div>
+        ))}
     </div>
   );
 }
